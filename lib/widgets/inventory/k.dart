72a5c157a822fa2b9065dd0af51a97a56497caec
AgusDim <agusdim321@gmail.com>
2023-02-24 18:33:08 +0700

    terbaru

--- lib/widgets/inventory/k.dart
+++ lib/widgets/inventory/k.dart
@@ -1,33 +1,33 @@
 class Coba {
-  String no;
-  String label;
-  String system;
-  String cabletype;
-  String manufacturer;
-  String amorigtype;
-  String coretype;
-  String core;
-  String length;
-  String inner;
-  String outer;
-  String tanklevel;
-  String remark;
-  String description;
+  String? no;
+  String? label;
+  String? system;
+  String? cabletype;
+  String? manufacturer;
+  String? amorigtype;
+  String? coretype;
+  String? core;
+  String? length;
+  String? inner;
+  String? outer;
+  String? tanklevel;
+  String? remark;
+  String? description;
 
   Coba({
-    required this.no,
-    required this.label,
-    required this.system,
-    required this.cabletype,
-    required this.manufacturer,
-    required this.amorigtype,
-    required this.coretype,
-    required this.core,
-    required this.length,
-    required this.inner,
-    required this.outer,
-    required this.tanklevel,
-    required this.remark,
-    required this.description,
+    this.no,
+    this.label,
+    this.system,
+    this.cabletype,
+    this.manufacturer,
+    this.amorigtype,
+    this.coretype,
+    this.core,
+    this.length,
+    this.inner,
+    this.outer,
+    this.tanklevel,
+    this.remark,
+    this.description,
   });
 }
