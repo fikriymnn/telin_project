03b656961011fe53184114f99d45350fd9cb28c3
ahmadriski23 <ahmadriskixrpl1csr@gmail.com>
2023-03-25 10:37:40 +0700

    connect to database

---
+++ lib/api/configAPI.dart
@@ -0,0 +1,14 @@
+String baseUrl = 'http://192.168.43.166:1000';
+
+// CRUD System
+String inputSystem = '$baseUrl/api/system/create';
+String editSystem = '$baseUrl/api/system/edit';
+String getAllSystem = '$baseUrl/api/system/getall';
+String hapusSystem = '$baseUrl/api/system/delete';
+
+// CRUD Manufacturer
+
+String inputManufacturer = '$baseUrl/api/manufacturer/create';
+String getAllManufacturer = '$baseUrl/api/manufacturer/getall';
+String hapusManufacturer = '$baseUrl/api/manufacturer/delete';
+String editManufacturer = '$baseUrl/api/manufacturer/edit';
--- lib/widgets/master_data/add_data/add_system.dart
+++ lib/widgets/master_data/add_data/add_system.dart
@@ -1,3 +1,6 @@
+import 'package:dio/dio.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -7,6 +10,8 @@ import 'package:telin_project/helpers/responsive.dart';
 
 import 'package:telin_project/widgets/setting/edit_akun.dart';
 
+import '../../../api/configAPI.dart';
+
 class AddSystem extends StatefulWidget {
   const AddSystem({super.key});
 
@@ -15,6 +20,12 @@ class AddSystem extends StatefulWidget {
 }
 
 class _AddSystemState extends State<AddSystem> {
+  TextEditingController txtNamaSystem = TextEditingController();
+
+  FocusNode focusNode = new FocusNode();
+
+  Response? response;
+  var dio = Dio();
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
@@ -23,105 +34,153 @@ class _AddSystemState extends State<AddSystem> {
           height: 588.6,
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(6), color: Colors.white),
-          child: SingleChildScrollView(
-              child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 30),
-            child: Column(
-              children: [
-                Text("Add New System",
-                    style: GoogleFonts.montserrat(
-                      fontSize: 23.3,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    )),
-                SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  width: 230,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        "System",
-                        style: GoogleFonts.montserrat(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
+          child: Form(
+            child: SingleChildScrollView(
+                child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 30),
+              child: Column(
+                children: [
+                  Text("Add New System",
+                      style: GoogleFonts.montserrat(
+                        fontSize: 23.3,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      )),
+                  SizedBox(
+                    height: 30,
                   ),
-                ),
-                Container(
-                  width: 230,
-                  height: 44,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(6),
-                      border: Border.all(width: 5, color: Color(0xffF0F0F0)),
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.black.withOpacity(0.25),
-                            blurRadius: 5,
-                            offset: Offset(0, 4))
-                      ]),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 18, bottom: 8),
-                    child: Center(
-                      child: TextField(
-                        style: GoogleFonts.montserrat(
-                          fontSize: 13.3,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black,
-                        ),
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintStyle: GoogleFonts.montserrat(
-                              fontSize: 13.3,
-                              fontWeight: FontWeight.w400,
-                              color: Colors.black,
-                            ),
-                            hintText: "System Name"),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 100,
-                ),
-                InkWell(
-                  onTap: () {
-                    QuickAlert.show(
-                context: context,
-                type: QuickAlertType.success,
-                text: 'Upload Data Success',
-               
-                width: 400,
-                
-                
-               confirmBtnColor: Colors.green
-              );
-                  },
-                  child: Container(
-                    width: 90,
-                    height: 37.3,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(6),
-                        color: Color(0xffEC1D26)),
-                    child: Center(
-                      child: Text("Submit",
-                          style: GoogleFonts.roboto(
+                  Container(
+                    width: 230,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "System",
+                          style: GoogleFonts.montserrat(
                             fontSize: 20,
                             fontWeight: FontWeight.w600,
-                            color: Colors.white,
-                          )),
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ),
-              ],
-            ),
-          ))),
+                  Container(
+                    width: 230,
+                    height: 44,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(6),
+                        border: Border.all(width: 5, color: Color(0xffF0F0F0)),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black.withOpacity(0.25),
+                              blurRadius: 5,
+                              offset: Offset(0, 4))
+                        ]),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 18, bottom: 8),
+                      child: Center(
+                        child: TextField(
+                          controller: txtNamaSystem,
+                          style: GoogleFonts.montserrat(
+                            fontSize: 13.3,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black,
+                          ),
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintStyle: GoogleFonts.montserrat(
+                                fontSize: 13.3,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black,
+                              ),
+                              hintText: "System Name"),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 100,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      if (txtNamaSystem.text == '') {
+                        QuickAlert.show(
+                            context: context,
+                            type: QuickAlertType.error,
+                            title: 'Peringatan',
+                            text: 'Nama System Tidak Boleh Kosong',
+                            width: 400,
+                            confirmBtnColor: Colors.red);
+                      } else {
+                        inputDataSystem(txtNamaSystem.text);
+                      }
+                    },
+                    child: Container(
+                      width: 90,
+                      height: 37.3,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(6),
+                          color: Color(0xffEC1D26)),
+                      child: Center(
+                        child: Text("Submit",
+                            style: GoogleFonts.roboto(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white,
+                            )),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )),
+          )),
     );
+  }
+
+  void _clearForm() {
+    txtNamaSystem.clear();
+  }
+
+  void inputDataSystem(namaSystem) async {
+    bool status;
+    var msg;
+    try {
+      // var formData = FormData.fromMap({
+      //   'system': namaSystem,
+      // });
+
+      response = await dio.post(inputSystem, data: {'system': namaSystem});
+      status = response!.data['sukses'];
+      msg = response!.data['msg'];
+      if (status) {
+        FocusScope.of(context).unfocus();
+        _clearForm();
+        QuickAlert.show(
+            context: context,
+            type: QuickAlertType.success,
+            text: '$msg',
+            width: 400,
+            confirmBtnColor: Colors.green);
+      } else {
+        QuickAlert.show(
+            context: context,
+            type: QuickAlertType.error,
+            text: '$msg',
+            title: 'Peringatan',
+            width: 400,
+            confirmBtnColor: Colors.red);
+      }
+    } catch (e) {
+      QuickAlert.show(
+          context: context,
+          type: QuickAlertType.error,
+          text: 'Terjadi Kesalahan Pada Server Kami',
+          title: 'Peringatan',
+          width: 400,
+          confirmBtnColor: Colors.red);
+    }
   }
 }
