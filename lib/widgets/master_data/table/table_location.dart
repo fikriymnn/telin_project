85210593c8441d173cf0ef66ac5f99ba25e111d7
Fikri Zaki Aditama <fikriyomann@student.upi.edu>
2023-03-27 19:00:27 +0700

    read data table location

--- lib/widgets/master_data/table/table_location.dart
+++ lib/widgets/master_data/table/table_location.dart
@@ -1,9 +1,11 @@
 import 'package:data_table_2/data_table_2.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:quickalert/quickalert.dart';
+import 'package:telin_project/api/configAPI.dart';
 import 'package:telin_project/constants/style.dart';
 import 'package:telin_project/widgets/home/detail_table_home.dart';
 import 'package:telin_project/widgets/master_data/edit_data/edit_location.dart';
@@ -17,21 +19,137 @@ class TableLocation extends StatefulWidget {
 }
 
 class _TableLocationState extends State<TableLocation> {
-  late List<Location> location;
- List <Location> selectedRow = [];
- @override
+  Response? response;
+
+  List location = [];
+
+  var dio = Dio();
+
+  @override
   void initState() {
     // TODO: implement initState
-    location = Location.getLocation();
+    getDataLocation();
     super.initState();
   }
+
+  DataRow _resultsAPI(index, data) {
+    return DataRow(cells: [
+      DataCell(Text('${index + 1}',
+          style: GoogleFonts.montserrat(
+            fontSize: 14.6,
+            fontWeight: FontWeight.w400,
+            color: Colors.black,
+          ))),
+      DataCell(Center(
+        child: Text('${data['location']}',
+            style: GoogleFonts.montserrat(
+              fontSize: 14.6,
+              fontWeight: FontWeight.w400,
+              color: Colors.black,
+            )),
+      )),
+      DataCell(Center(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () {
+                showDialog(
+                    context: context,
+                    barrierColor: Colors.transparent,
+                    builder: (BuildContext context) {
+                      return EditLocation();
+                    });
+              },
+              child: Container(
+                width: 50,
+                height: 19.46,
+                decoration: BoxDecoration(
+                    color: green, borderRadius: BorderRadius.circular(6)),
+                child: Center(
+                    child: Text("Edit",
+                        style: GoogleFonts.montserrat(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white,
+                        ))),
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            InkWell(
+              onTap: () {
+                QuickAlert.show(
+                  context: context,
+                  type: QuickAlertType.confirm,
+                  text: 'Do you sure to delete this item',
+                  confirmBtnText: 'Yes',
+                  cancelBtnText: 'No',
+                  customAsset: 'assets/gift/error.gif',
+                  width: 400,
+                  confirmBtnColor: Colors.green,
+                );
+              },
+              child: Container(
+                width: 50,
+                height: 19.46,
+                decoration: BoxDecoration(
+                    color: active, borderRadius: BorderRadius.circular(6)),
+                child: Center(
+                    child: Text("Delete",
+                        style: GoogleFonts.montserrat(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white,
+                        ))),
+              ),
+            ),
+          ],
+        ),
+      )),
+    ]);
+  }
+
+  void getDataLocation() async {
+    bool status;
+    var msg;
+    try {
+      response = await dio.get(getAllLocation);
+      status = response!.data['sukses'];
+      msg = response!.data['msg'];
+      if (status) {
+        setState(() {
+          location = response!.data['data'];
+        });
+      } else {
+        QuickAlert.show(
+            context: context,
+            type: QuickAlertType.error,
+            text: '$msg',
+            title: 'Peringatan',
+            width: 400,
+            confirmBtnColor: Colors.red);
+      }
+    } catch (e) {
+      QuickAlert.show(
+          context: context,
+          type: QuickAlertType.error,
+          text: 'Terjadi Kesalahan Pada Server Kami',
+          title: 'Peringatan',
+          width: 400,
+          confirmBtnColor: Colors.red);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return DataTable2(
         columnSpacing: 6,
         horizontalMargin: 6,
         dataRowHeight: 30,
-            border: TableBorder(top: BorderSide(), bottom: BorderSide()),
+        border: TableBorder(top: BorderSide(), bottom: BorderSide()),
         columns: [
           DataColumn2(
             label: Text(
@@ -57,117 +175,7 @@ class _TableLocationState extends State<TableLocation> {
             label: Text(''),
           ),
         ],
-        rows: _createRowsManufacture()
-                );
-  }
- List<DataRow> _createRowsManufacture() {
-    return location
-        .map((location) => DataRow(
-          
-          cells: [
-              DataCell(Text(location.no,
-                      style: GoogleFonts.montserrat(
-                        fontSize: 14.6,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.black,
-                      ))),
-                  DataCell(Center(
-                    child: Text(location.locationName,
-                        style: GoogleFonts.montserrat(
-                          fontSize: 14.6,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black,
-                        )),
-                  )),
-                  DataCell(Center(
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        InkWell(
-                          onTap: () {
-                           showDialog(
-                          context: context,
-                          barrierColor: Colors.transparent,
-                          builder: (BuildContext context) {
-                            return EditLocation();
-                          });
-                          },
-                          child: Container(
-                            width: 50,
-                            height: 19.46,
-                             decoration: BoxDecoration(
-                                color: green,
-                                borderRadius: BorderRadius.circular(6)),
-                            child: Center(
-                              child: Text("Edit",style: GoogleFonts.montserrat(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white,
-                        ))
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        InkWell(
-                          onTap: () {
-                            QuickAlert.show(
-                          context: context,
-                          type: QuickAlertType.confirm,
-                          text: 'Do you sure to delete this item',
-                          confirmBtnText: 'Yes',
-                          cancelBtnText: 'No',
-                          customAsset: 'assets/gift/error.gif',
-                          width: 400,
-                          confirmBtnColor: Colors.green,
-                          
-                        );
-                          },
-                          child: Container(
-                            width: 50,
-                            height: 19.46,
-                             decoration: BoxDecoration(
-                                color: active,
-                                borderRadius: BorderRadius.circular(6)),
-                            child: Center(
-                              child: Text("Delete",style: GoogleFonts.montserrat(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white,
-                        ))
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  )),
-            ]))
-        .toList();
+        rows: List.generate(
+            location.length, (index) => _resultsAPI(index, location[index])));
   }
 }
-
-
-
-
-
-class Location {
-    final String no,locationName;
-
-    const Location({
-      required this.no,
-      required this.locationName,
-     
-    });
-
-    static List<Location> getLocation(){
-      return <Location>[
-        Location(no: "1", locationName: "Location", ),
-        Location(no: "2", locationName: "Location", ),
-        Location(no: "3", locationName: "Location",  ),
-        Location(no: "4", locationName: "Location",  ),
-        Location(no: "5", locationName: "Location", )
-      ];
-    }
-  }
