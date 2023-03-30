9984e8d4ffc648d12ec123bb2931f0c9df042135
acepW <acepwahyu001@gmail.com>
2023-03-30 19:40:53 +0700

    -

--- lib/widgets/report/print_report.dart
+++ lib/widgets/report/print_report.dart
@@ -23,19 +23,6 @@ class printReport {
     }
 
     ///
-    /// Change sheet from rtl to ltr and vice-versa i.e. (right-to-left -> left-to-right and vice-versa)
-    ///
-    var sheet1rtl = excel['Sheet1'].isRTL;
-    excel['Sheet1'].isRTL = false;
-    print(
-        'Sheet1: ((previous) isRTL: $sheet1rtl) ---> ((current) isRTL: ${excel['Sheet1'].isRTL})');
-
-    var sheet2rtl = excel['Sheet2'].isRTL;
-    excel['Sheet2'].isRTL = true;
-    print(
-        'Sheet2: ((previous) isRTL: $sheet2rtl) ---> ((current) isRTL: ${excel['Sheet2'].isRTL})');
-
-    ///
     ///
     /// declaring a cellStyle object
     ///
@@ -58,65 +45,6 @@ class printReport {
     cell2.value = "Heya How night";
     cell2.cellStyle = cellStyle;
 
-    /// printing cell-type
-    print("CellType: " + cell.cellType.toString());
-
-    ///
-    ///
-    /// Iterating and changing values to desired type
-    ///
-    ///
-    for (int row = 0; row < sheet.maxRows; row++) {
-      sheet.row(row).forEach((Data? cell1) {
-        if (cell1 != null) {
-          cell1.value = ' My custom Value ';
-        }
-      });
-    }
-
-    excel.rename("mySheet", "myRenamedNewSheet");
-
-    var sheet1 = excel['Sheet1'];
-    sheet1.cell(CellIndex.indexByString('A1')).value = 'Sheet1';
-
-    /// fromSheet should exist in order to sucessfully copy the contents
-    excel.copy('Sheet1', 'newlyCopied');
-
-    var sheet2 = excel['newlyCopied'];
-    sheet2.cell(CellIndex.indexByString('A1')).value = 'Newly Copied Sheet';
-
-    /// renaming the sheet
-    excel.rename('oldSheetName', 'newSheetName');
-
-    /// deleting the sheet
-    excel.delete('Sheet1');
-
-    /// unlinking the sheet if any link function is used !!
-    excel.unLink('sheet1');
-
-    sheet = excel['sheet'];
-
-    /// appending rows and checking the time complexity of it
-    Stopwatch stopwatch = Stopwatch()..start();
-    List<List<String>> list = List.generate(
-        9000, (index) => List.generate(20, (index1) => '$index $index1'));
-
-    print('list creation executed in ${stopwatch.elapsed}');
-    stopwatch.reset();
-    list.forEach((row) {
-      sheet.appendRow(row);
-    });
-    print('appending executed in ${stopwatch.elapsed}');
-
-    sheet.appendRow([8]);
-    bool isSet = excel.setDefaultSheet(sheet.sheetName);
-    // isSet is bool which tells that whether the setting of default sheet is successful or not.
-    if (isSet) {
-      print("${sheet.sheetName} is set to default sheet.");
-    } else {
-      print("Unable to set ${sheet.sheetName} to default sheet.");
-    }
-
     var colIterableSheet = excel['ColumnIterables'];
 
     var colIterables = ['A', 'B', 'C', 'D', 'E'];
@@ -133,14 +61,5 @@ class printReport {
     // Saving the file
 
     String outputFile = "/Users/kawal/Desktop/git_projects/r.xlsx";
-
-    //stopwatch.reset();
-    List<int>? fileBytes = excel.save();
-    //print('saving executed in ${stopwatch.elapsed}');
-    if (fileBytes != null) {
-      File(join(outputFile))
-        ..createSync(recursive: true)
-        ..writeAsBytesSync(fileBytes);
-    }
   }
 }
