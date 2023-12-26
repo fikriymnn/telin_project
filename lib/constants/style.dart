9f70bf77c367849b6e88cb6c23a004dbcdb74b85
acepW <acepwahyu001@gmail.com>
2023-12-26 15:51:05 +0700

    add color bg grey and active table

--- lib/constants/style.dart
+++ lib/constants/style.dart
@@ -5,3 +5,5 @@ Color lightGrey = const Color(0xFFA4A6B3);
 Color dark = const Color(0xFF363740);
 Color active = const Color(0xFFED1D25);
 Color green = const Color(0xffB1CC85);
+Color bgGray = Color(0xFFE9E9E9);
+Color activeTable = Color(0xffFABBBE);
